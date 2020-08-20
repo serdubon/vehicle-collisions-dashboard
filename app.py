857b61de32a86589675c3e8f8de8b91e10655bbb
serdubon <serdubon2162@gmail.com>
2020-08-19 22:15:06 -0600

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,6 @@ import pydeck as pdk
 import plotly.express as px
 
 
-st.title("Motor Vehicle Collisions in New York City")
-st.markdown("Application to visualize car accidents in the city of New York. ")
-
 # Geting the data and keep the data in cache
 @st.cache(persist=True)
 def load_data(nrows):
@@ -23,108 +20,116 @@ def load_data(nrows):
     return data
 
 
-# Loading the data
-data = load_data(100000)
-original_data = data
+def main():
+    st.title("Motor Vehicle Collisions in New York City")
+    st.markdown("Application to visualize car accidents in the city of New York. ")
 
-st.header("Where are the most people injured in NYC?")
-injured_people = st.slider("Number of people injured in car accident", 0, 19, 3)
+    # Loading the data
+    data = load_data(100000)
+    original_data = data
 
-# Map of positions by number of people injured
-st.map(
-    data[data["number of persons injured"] >= injured_people][
-        ["latitude", "longitude"]
-    ].dropna(how="any")
-)
+    st.header("Where are the most people injured in NYC?")
+    injured_people = st.slider("Number of people injured in car accident", 0, 19, 3)
 
-# Filter number of colissions by hour of the day
-st.header("How many collisions occure during a givin time in NYC?")
-hour = st.slider("Hour to look at", 0, 23)
-data = data[data["date/time"].dt.hour == hour]
-
-st.markdown("Vehicles collision between %i:00 and %i:00" % (hour, hour + 1))
-
-midpoint = (np.average(data["latitude"]), np.average(data["longitude"]))
-
-st.pydeck_chart(
-    pdk.Deck(
-        map_style="mapbox://styles/mapbox/light-v9",
-        initial_view_state={
-            "latitude": midpoint[0],
-            "longitude": midpoint[1],
-            "zoom": 12,
-            "pitch": 50,
-        },
-        layers=[
-            pdk.Layer(
-                "HexagonLayer",
-                data=data[["date/time", "latitude", "longitude"]],
-                get_position=["longitude", "latitude"],
-                radius=75,
-                extruded=True,
-                pickable=True,
-                elevation_scale=4,
-                elevation_range=[0, 1000],
-            ),
-        ],
+    # Map of positions by number of people injured
+    st.map(
+        data[data["number of persons injured"] >= injured_people][
+            ["latitude", "longitude"]
+        ].dropna(how="any")
     )
-)
 
-# Filter number of accidents by minutes between to hours
-st.header("Breackdown by minute between %i:00 and %i:00" % (hour, hour + 1))
-filtered_data = data[
-    (data["date/time"].dt.hour >= hour) & (data["date/time"].dt.hour < (hour + 1))
-]
-histogram = np.histogram(filtered_data["date/time"].dt.minute, bins=60)[0]
-chart_data = pd.DataFrame({"minute": range(60), "crashes": histogram})
-st.write(
-    px.bar(
-        chart_data,
-        x="minute",
-        y="crashes",
-        hover_data=["minute", "crashes"],
-        height=400,
-        range_x=(0, 60),
+    # Filter number of colissions by hour of the day
+    st.header("How many collisions occure during a givin time in NYC?")
+    hour = st.slider("Hour to look at", 0, 23)
+    data = data[data["date/time"].dt.hour == hour]
+
+    st.markdown("Vehicles collision between %i:00 and %i:00" % (hour, hour + 1))
+
+    midpoint = (np.average(data["latitude"]), np.average(data["longitude"]))
+
+    st.pydeck_chart(
+        pdk.Deck(
+            map_style="mapbox://styles/mapbox/light-v9",
+            initial_view_state={
+                "latitude": midpoint[0],
+                "longitude": midpoint[1],
+                "zoom": 12,
+                "pitch": 50,
+            },
+            layers=[
+                pdk.Layer(
+                    "HexagonLayer",
+                    data=data[["date/time", "latitude", "longitude"]],
+                    get_position=["longitude", "latitude"],
+                    radius=75,
+                    extruded=True,
+                    pickable=True,
+                    elevation_scale=4,
+                    elevation_range=[0, 1000],
+                ),
+            ],
+        )
     )
-)
 
-# Filter the top dangerous streets by affected type
-st.header("Top 5 dangerous streets by affected type")
-select = st.selectbox(
-    "Affected type of people", ["Pedestrians", "Cyclist", "Motorists"]
-)
-
-if select == "Pedestrians":
+    # Filter number of accidents by minutes between to hours
+    st.header("Breackdown by minute between %i:00 and %i:00" % (hour, hour + 1))
+    filtered_data = data[
+        (data["date/time"].dt.hour >= hour) & (data["date/time"].dt.hour < (hour + 1))
+    ]
+    histogram = np.histogram(filtered_data["date/time"].dt.minute, bins=60)[0]
+    chart_data = pd.DataFrame({"minute": range(60), "crashes": histogram})
     st.write(
-        original_data[original_data["number of pedestrians injured"] >= 1][
-            ["on street name", "number of pedestrians injured"]
-        ]
-        .sort_values(by=["number of pedestrians injured"], ascending=False)
-        .dropna(how="any")[:5]
-    )
-elif select == "Cyclist":
-    st.write(
-        original_data[original_data["number of cyclist injured"] >= 1][
-            ["on street name", "number of cyclist injured"]
-        ]
-        .sort_values(by=["number of cyclist injured"], ascending=False)
-        .dropna(how="any")[:5]
-    )
-else:
-    st.write(
-        original_data[original_data["number of motorist injured"] >= 1][
-            ["on street name", "number of motorist injured"]
-        ]
-        .sort_values(by=["number of motorist injured"], ascending=False)
-        .dropna(how="any")[:5]
+        px.bar(
+            chart_data,
+            x="minute",
+            y="crashes",
+            hover_data=["minute", "crashes"],
+            height=400,
+            range_x=(0, 60),
+        )
     )
 
-# Show the original data in a table
-if st.checkbox("Show Raw Data", False):
-    st.subheader("Raw Data")
-    st.write(data.head(300))
+    # Filter the top dangerous streets by affected type
+    st.header("Top 5 dangerous streets by affected type")
+    select = st.selectbox(
+        "Affected type of people", ["Pedestrians", "Cyclist", "Motorists"]
+    )
 
-st.markdown(
-    'Made with *Love!*  <a href="https://github.com/serdubon"> <img src="https://img.icons8.com/material-rounded/24/000000/github.png"/> </a> :heartbeat:',
-    unsafe_allow_html=True,
-)
+    if select == "Pedestrians":
+        st.write(
+            original_data[original_data["number of pedestrians injured"] >= 1][
+                ["on street name", "number of pedestrians injured"]
+            ]
+            .sort_values(by=["number of pedestrians injured"], ascending=False)
+            .dropna(how="any")[:5]
+        )
+    elif select == "Cyclist":
+        st.write(
+            original_data[original_data["number of cyclist injured"] >= 1][
+                ["on street name", "number of cyclist injured"]
+            ]
+            .sort_values(by=["number of cyclist injured"], ascending=False)
+            .dropna(how="any")[:5]
+        )
+    else:
+        st.write(
+            original_data[original_data["number of motorist injured"] >= 1][
+                ["on street name", "number of motorist injured"]
+            ]
+            .sort_values(by=["number of motorist injured"], ascending=False)
+            .dropna(how="any")[:5]
+        )
+
+    # Show the original data in a table
+    if st.checkbox("Show Raw Data", False):
+        st.subheader("Raw Data")
+        st.write(data.head(300))
+
+    st.markdown(
+        'Made with *Love!*  <a href="https://github.com/serdubon"> <img src="https://img.icons8.com/material-rounded/24/000000/github.png"/> </a> :heartbeat:',
+        unsafe_allow_html=True,
+    )
+
+
+if __name__ == "__main__":
+    main()
